03b1d4c7d14583faa40aa4c3a16625930050ee61
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-05 20:11:07 -0700

    api/category findById DELETE route created

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -61,3 +61,22 @@ router.put('/:id', (req, res) => {
       res.status(400).json(err);
     });
 });
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id }
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+});
+
+module.exports = router;
