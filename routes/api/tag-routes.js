04d5b8ba80c21b71dabb1f5b3bd4752388699ab9
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-05 20:17:39 -0700

    api/tag-routes GET FindById route created

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,3 +16,22 @@ router.get('/', async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: "products" }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No Tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
