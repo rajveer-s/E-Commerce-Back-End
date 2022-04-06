f3b3a9e0280d19fb898951a3ac61f78ad1bcb3e2
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-05 20:09:48 -0700

    api/category findById get route created

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,3 +15,22 @@ router.get('/', async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
