f5e46c6b489e3944b7dc84ed6dac63e96cc5c21f
Rajveer Singh <rajsidhu2924@gmail.com>
2022-04-05 20:10:20 -0700

    api/category findById POST route created

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,3 +34,13 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
